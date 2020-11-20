1c84211d66b9ff11f033895271f3ebc447c773d4
mizugakun <mizugakun@gmail.com>
2020-11-19 21:49:49 -0500

    Repository will update the live date by postValue method through a timer which is for refreshing the data per second

--- app/src/main/java/Repositories/TimerListRepository.java
+++ app/src/main/java/Repositories/TimerListRepository.java
@@ -8,6 +8,8 @@ import androidx.lifecycle.MutableLiveData;
 import com.example.cs6011_project.AbsTimer;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import data.TimerData;
 import utilities.FileHelper;
@@ -16,14 +18,26 @@ import utilities.TimerHelper;
 public class TimerListRepository {
     private Application app;
     public MutableLiveData<List<AbsTimer>> timers;
-
+    Timer refresher;
     public TimerListRepository(@NonNull Application application) {
         app = application;
         timers = new MutableLiveData<>();
 
         String jsonString = FileHelper.getTextFromAssets(app,"Timers.json");
         List<AbsTimer> data = FileHelper.ParseHelper(jsonString);
-        timers.setValue(data);
         TimerHelper.StartCounting(data);
+        timers.setValue(data);
+        updateValue();
+    }
+
+    private void updateValue() {
+        refresher = new Timer();
+        TimerTask refreshUI = new TimerTask() {
+            @Override
+            public void run() {
+                timers.postValue(timers.getValue());
+            }
+        };
+        refresher.scheduleAtFixedRate(refreshUI, 0, 1000);
     }
 }
